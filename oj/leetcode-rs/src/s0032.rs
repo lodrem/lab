ad3944558d3234ce22060ea30311bfb8634e871c
ccc13 <currantxx@gmail.com>
2019-12-15 16:19:05 +0800

    Add another solution for 32

--- oj/leetcode-rs/src/s0032.rs
+++ oj/leetcode-rs/src/s0032.rs
@@ -3,6 +3,57 @@ pub struct Solution;
 #[allow(dead_code)]
 impl Solution {
     pub fn longest_valid_parentheses(s: String) -> i32 {
+        Self::solve_by_counting(s)
+    }
+
+    fn solve_by_counting(s: String) -> i32 {
+        let bytes = s.as_bytes();
+        let mut max_len = 0;
+
+        {
+            // from left to right
+            let mut left = 0;
+            let mut right = 0;
+
+            for i in 0..bytes.len() {
+                match bytes[i] {
+                    b'(' => left += 1,
+                    _ => right += 1,
+                }
+
+                if left == right {
+                    max_len = std::cmp::max(max_len, left + right);
+                } else if right > left {
+                    left = 0;
+                    right = 0;
+                }
+            }
+        }
+
+        {
+            // from right to left
+            let mut left = 0;
+            let mut right = 0;
+
+            for i in (0..bytes.len()).rev() {
+                match bytes[i] {
+                    b'(' => left += 1,
+                    _ => right += 1,
+                }
+
+                if left == right {
+                    max_len = std::cmp::max(max_len, left + right);
+                } else if right < left {
+                    left = 0;
+                    right = 0;
+                }
+            }
+        }
+
+        max_len
+    }
+
+    fn solve_by_stack(s: String) -> i32 {
         use std::collections::VecDeque;
 
         let bytes = s.as_bytes();
